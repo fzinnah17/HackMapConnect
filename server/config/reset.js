78a03136a9d778fd0183d99009fb9223d42da6b9
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-05 08:50:22 -0400

    created reset.js file

---
+++ server/config/reset.js
@@ -0,0 +1,33 @@
+import { pool } from './database.js';
+
+const createTables = async () => {
+    try {
+        // Create locations table
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS locations (
+                id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL UNIQUE
+            );
+        `);
+
+        // Create events table
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS events (
+                id SERIAL PRIMARY KEY,
+                location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE,
+                title TEXT NOT NULL,
+                description TEXT,
+                date TIMESTAMP NOT NULL
+            );
+        `);
+
+        console.log("Tables created successfully!");
+
+    } catch (err) {
+        console.error("Error creating tables: ", err);
+    } finally {
+        pool.end();
+    }
+};
+
+createTables();
