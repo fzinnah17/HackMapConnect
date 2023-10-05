63cdbd00199eac9548e765a06e5ae8d011112756
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-05 08:54:38 -0400

    locations.js file

--- server/controllers/locations.js
+++ server/controllers/locations.js
@@ -1,2 +1,13 @@
 // Fetch all locations.
 // Maybe later: Add, Update, and Delete locations.
+
+import { pool } from '../database.js';
+
+export const getAllLocations = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM locations');
+        res.status(200).json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch locations.' });
+    }
+};
