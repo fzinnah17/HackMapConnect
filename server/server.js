a194364178ee0fe42d74495271f641602a1e3dae
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-05 09:10:57 -0400

    evetns & locations API routes in express server

--- server/server.js
+++ server/server.js
@@ -1,37 +1,35 @@
-import express from 'express'
-import path from 'path'
-import favicon from 'serve-favicon'
-import dotenv from 'dotenv'
-import { getAllEvents, getEventsByLocation } from './controllers/events.js';
-import { getAllLocations } from './controllers/locations.js';
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import dotenv from 'dotenv';
+import eventsRoutes from './routes/eventsRoutes.js';
+import locationsRoutes from './routes/locationsRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // API routes
-app.get('/api/events', getAllEvents);
-app.get('/api/events/location/:id', getEventsByLocation);
-app.get('/api/locations', getAllLocations);
+app.use('/api/events', eventsRoutes);
+app.use('/api/locations', locationsRoutes);
 
 if (process.env.NODE_ENV === 'development') {
-    app.use(favicon(path.resolve('../', 'client', 'public', 'party.png')))
-}
-else if (process.env.NODE_ENV === 'production') {
-    app.use(favicon(path.resolve('public', 'party.png')))
-    app.use(express.static('public'))
+    app.use(favicon(path.resolve('../', 'client', 'public', 'party.png')));
+} else if (process.env.NODE_ENV === 'production') {
+    app.use(favicon(path.resolve('public', 'party.png')));
+    app.use(express.static('public'));
 }
 
 if (process.env.NODE_ENV === 'production') {
     app.get('/*', (_, res) =>
         res.sendFile(path.resolve('public', 'index.html'))
-    )
+    );
 }
 
 app.listen(PORT, () => {
-    console.log(`server listening on http://localhost:${PORT}`)
-})
+    console.log(`server listening on http://localhost:${PORT}`);
+});
