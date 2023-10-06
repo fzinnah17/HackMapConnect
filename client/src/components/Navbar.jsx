923f0fd9ddd35f9024acde44dfec1f7bd96d5b11
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-06 19:05:26 -0400

    Navbar added with style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../css/Navbar.css"
 
 const Navbar = () => {
     return (
-        <nav>
-            <h1>Hack Map Connect</h1>
-            <Link to="/">Home</Link>
-            <Link to="/all-events">All Events</Link>
+        <nav className="nav-container bg-light">
+            <Link className="brand" to="/">Hack Map Connect</Link>
+            <div className="collapse" id="navLinks">
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/all-events">All Events</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 };
